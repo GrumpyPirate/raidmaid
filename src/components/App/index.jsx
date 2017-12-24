ed27eda4b6d345a29267232788f80ae5b6f470f5
Edward Cobbold <edward.cobbold@gmail.com>
2017-12-24 00:35:34 +0000

    cra: eject; add postcss-css-next build

---
+++ src/components/App/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import classes from './index.css'
+
+class App extends Component {
+  render() {
+    return (
+      /* Current Raid list */
+      <div className={classes.App}>
+        <h1 style={{ margin: 0 }}>Testicles</h1>
+      </div>
+      /* Report CTAs */
+    );
+  }
+}
+
+export default App;
